dead109b2d84ed1d1d1f8b34f2184636486d1796
FabianRivera <84300206+WhyDoIHaveToPick@users.noreply.github.com>
2024-05-09 17:25:08 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -64,34 +64,38 @@ let typingTimeout; // Variable to store the timeout ID
 
 
 function typeWriter() {
-    if (currentText < texts.length) {
-        // Typing texts array
-        if (index < texts[currentText].length && isTypingActive) {
-            element.innerHTML += texts[currentText].charAt(index);
-            index++;
-            typingTimeout = setTimeout(typeWriter, typingSpeed);
-        } else {
-            // Switching to the next text after typing completion
-            currentText++;
-            index = 0;
-            // Delay before starting the next text
-            typingTimeout = setTimeout(typeWriter, 500);
-        }
-    } else if (currentText === texts.length && index < monkeyText[0].length && isTypingActive) {
-        // Typing monkeyText after completing texts
-        element.innerHTML += monkeyText[0].charAt(index);
-        index++;
-        typingTimeout = setTimeout(typeWriter, typingSpeed);
-    } else if (currentText === texts.length && index >= monkeyText[0].length && index < texts_final_type[0].length && isTypingActive) {
-        // Typing final text after completing monkeyText
-        element.innerHTML += texts_final_type[0].charAt(index);
-        index++;
-        typingTimeout = setTimeout(typeWriter, typingSpeed);
-    } else {
-        // No more text to type
-        isTypingActive = false;
-    }
+// Check if we're still typing the current text
+if (index < texts[currentText].length && isTypingActive) {
+element.innerHTML += texts[currentText].charAt(index);
+index++;
+typingTimeout = setTimeout(typeWriter, typingSpeed);
+} else {
+// If we've finished typing the current text
+if (currentText < texts.length - 1) {
+// Move to the next text in the array
+currentText++;
+index = 0;
+// A short delay before starting the next text
+typingTimeout = setTimeout(typeWriter, 500);
+} else if (currentText === texts.length - 1 && index < monkeyText[0].length) {
+// Start typing monkeyText after texts is done
+element.innerHTML += monkeyText[0].charAt(index);
+index++;
+typingTimeout = setTimeout(typeWriter, typingSpeed);
+} else if (currentText === texts.length - 1 && index >= monkeyText[0].length && index < texts_final_type[0].length) {
+// Start typing texts_final_type after monkeyText is done
+element.innerHTML += texts_final_type[0].charAt(index);
+index++;
+typingTimeout = setTimeout(typeWriter, typingSpeed);
+} else {
+// Typing is complete for all texts
+isTypingActive = false;
+// Optionally, you can reset the typewriter or perform other actions here
 }
+}
+}
+
+
 
 function startTypewriter() {
     typeWriter();
