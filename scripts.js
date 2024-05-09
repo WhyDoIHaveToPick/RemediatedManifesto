3115cc289da99bd93d3f72f6d855a665e9235ce8
FabianRivera <84300206+WhyDoIHaveToPick@users.noreply.github.com>
2024-05-09 18:12:18 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,39 +3,39 @@ const monkeyText = [
     `
 
     
-		                 	 H
-
-                            E		I T		E
-                                         B
-                     L                  U                     L
-		     
-                P		         N		         P
-                                         S
-                                       Y O U
-                                      U Y O Y
-			CHEMICALS   O W U E O Y THE WHIP SNAPS
-                                      U O Y U     PAIN. BL
-                            BE(little) Y O U               OOD
-                   PP                    S                    PP
-                                         C
-                        L             R     E             L
-                                  a      m      s
-                               a     h       h     h
-                                         ;
-                                      SILENCE
-                                E      POWER      E
-                                      L e s s
-                                         H
-                                      (D* RI
-                                    PPPPIR D PPPPP
-                                        DRI* RIPIIIII
-                                       P*  I P PPPP P)
-
-
-
-                                WE demand sadness befall
-                                        U.S
-                                NO                LONGER
+									                 	 H
+							
+							                            E		I T		E
+							                                         B
+							                     L                  U                     L
+									     
+							                P		         N		         P
+							                                         S
+							                                       Y O U
+							                                      U Y O Y
+										CHEMICALS   O W U E O Y THE WHIP SNAPS
+							                                      U O Y U     PAIN. BL
+							                            BE(little) Y O U               OOD
+							                   PP                    S                    PP
+							                                         C
+							                        L             R     E             L
+							                                  a      m      s
+							                               a     h       h     h
+							                                         ;
+							                                      SILENCE
+							                                E      POWER      E
+							                                      L e s s
+							                                         H
+							                                      (D* RI
+							                                    PPPPIR D PPPPP
+							                                        DRI* RIPIIIII
+							                                       P*  I P PPPP P)
+							
+							
+							
+							                                WE demand sadness befall
+							                                        U.S
+							                                NO                LONGER
                     
 
 
